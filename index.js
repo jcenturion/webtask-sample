6752a7eefa57f7e2f68a54f2a792d30304d0072d
centurion.javier@gmail.com <centurion.javier@gmail.com>
2016-07-05 20:33:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Request = require("superagent")
 var url = 'https://api.github.com/repos/jcenturion/webtask-sample/contents/README.md?ref=master'
     
 module.exports = function(ctx, cb) {
+  // TODO: Refactor
   Request
     .get(url)
     .end((err, res) => {
