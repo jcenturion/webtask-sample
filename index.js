9aeaaba853373b89711449feb9b46ed6a423ba75
centurion.javier@gmail.com <centurion.javier@gmail.com>
2016-07-05 03:19:02 -0300

    Update index.js, yeah.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-"use latest";
+var Request = require("superagent")
+var url = 'https://api.github.com/repos/jcenturion/webtask-sample/contents/README.md?ref=master'
+var Buffer = require('buffer').Buffer;
+    
+module.exports = function(ctx, cb) {
+  var buffer = new Buffer('hola mundo');
+  
+  console.log(buffer.toString('base64'));
+  
+  cb(null);
+  
+  // Request
+  //   .get(url)
+  //   .end((err, res) => {
+  //     if (err) {
+  //       return cb(err);
+  //     }
 
-/* Enable latest ESvNext features through babel with "use latest";
-   at the top of the script. */
-
-module.exports = cb => cb(null, "Welcome to ES6 arrows!");
+  //     Request
+  //       .get(res.body.download_url)
+  //       .end((err, res) => {
+  //         if (err) {
+  //           return cb(err);
+  //         }
+          
+  //         cb(null, res.text);
+  //       });
+  //   });
+}
