cccec014ac5c5634cb17f9c92b05bf8738288344
Javier Centurion <centurion.javier@gmail.com>
2016-12-05 20:50:31 -0300

    Create new file

---
+++ cron-sample-js.js
@@ -0,0 +1,23 @@
+var Request = require("superagent")
+var url = 'https://api.github.com/repos/jcenturion/webtask-sample/contents/README.md?ref=master'
+    
+module.exports = function(ctx, cb) {
+  // TODO: Pull request change
+  Request
+    .get(url)
+    .end((err, res) => {
+      if (err) {
+        return cb(err);
+      }
+
+      Request
+        .get(res.body.download_url)
+        .end((err, res) => {
+          if (err) {
+            return cb(err);
+          }
+          
+          cb(null, res.text);
+        });
+    });
+}
