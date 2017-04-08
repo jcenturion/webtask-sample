24b58c29f15392bc0913948814a61c2ddb0ab35c
Javier Centurion <centurion.javier@gmail.com>
2017-04-08 00:04:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function (require) {
+function (require) {
   var express = require('express');
   var app = express();
   
@@ -7,4 +7,4 @@ module.exports = function (require) {
   });
   
   return app;
-};
+}
