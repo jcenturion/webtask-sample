b6de15f3e299bdce1f3ebc9cb18ac6faca7ef4ba
Javier Centurion <centurion.javier@gmail.com>
2017-06-28 20:28:13 -0300

    Updating index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-var Request = require("superagent")
-var url = 'https://api.github.com/repos/jcenturion/webtask-sample/contents/README.md?ref=master'
-    
-module.exports = function(ctx, cb) {
-  // TODO: 
-  Request
-    .get(url)
-    .end((err, res) => {
-      if (err) {
-        return cb(err);
-      }
-
-      Request
-        .get(res.body.download_url)
-        .end((err, res) => {
-          if (err) {
-            return cb(err);
-          }
-          
-          cb(null, res.text);
-        });
-    });
-}
+module.exports = function(context, cb) {
+  cb(null, { hello: context.data.name || 'Anonymous' });
+};  
